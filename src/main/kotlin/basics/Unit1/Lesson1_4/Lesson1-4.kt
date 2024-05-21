236da166f8dc27c378ec3854791355fa4f986122
Falon <47285315+falOn-Dev@users.noreply.github.com>
2024-05-20 22:01:43 -0400

    Removed old lessons

--- src/main/kotlin/basics/Unit1/Lesson1_4/Lesson1-4.kt
+++
@@ -1,8 +0,0 @@
-package basics.Unit1.Lesson1_4
-
-fun main(){
-    /*
-    - Create an array of 5 integers
-    - Create a for loop that iterates through the array and prints out each value
-     */
-}
